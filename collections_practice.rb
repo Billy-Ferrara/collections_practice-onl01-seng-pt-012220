2c3cf4996f0619cfd8d3fc45acd6913e82c71392
Billy Ferrara <willfira717@gmail.com>
2020-01-31 21:50:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,6 @@ def sort_array_asc(array)
   array.sort
 end
 
-sort_array_asc([25, 7, 1]) #[1,7,25]
 
 def sort_array_desc(array)
   array.sort do | left, right|
@@ -10,28 +9,20 @@ def sort_array_desc(array)
   end
 end
 
-sort_array_desc([25, 7, 14]) 
-
 def sort_array_char_count(array)
   array.sort do |left, right|
     left.length <=> right.length
   end
 end
 
-sort_array_char_count(["dogs", "cat", "Horses"]) 
-
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
 
-swap_elements(["blake", "ashley", "scott"]) 
-
 def reverse_array(array)
   array.reverse
 end
-
-reverse_array(["blake", "ashley", "scott"])
 
 def kesha_maker(array)
   array.each do |item|
@@ -39,16 +30,12 @@ def kesha_maker(array)
   end
 end
 
-kesha_maker(["blake", "ashley", "scott"]) 
-
 def find_a(array)
   array.find_all do |word|
     word[0] == "a"
   end
 
 end
-
-find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) 
 
 def sum_array(array)
   sum = 0
@@ -60,8 +47,6 @@ end
  
 end
 
-sum_array([11,4,7,8,9,100,134])
-
 def add_s(array)
   array.collect do |word|
     if array[1] == word
@@ -71,5 +56,3 @@ def add_s(array)
     end
   end
 end
-
-add_s(["hand","feet", "knee", "table"]) 
